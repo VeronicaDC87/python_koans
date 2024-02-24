e5ef651d282207f96477960ee5a54b4445a7630c
VeronicaDC87 <152705316+VeronicaDC87@users.noreply.github.com>
2024-02-24 17:17:19 +0000

    test 2008

--- src/is_leap_year.py
+++ src/is_leap_year.py
@@ -1,6 +1,6 @@
 def is_leap_year(year):
     if year == 2012:
         return True
-    if year == 1996:
+    if year == 2008:
         return True
     return False
--- src/test_leap_year.py
+++ src/test_leap_year.py
@@ -5,6 +5,6 @@ def test_is_not_leap_year():
     assert is_leap_year(1957) == False 
 
 def test_is_divisible_by_4():
-    assert is_leap_year(1996) == True 
+    assert is_leap_year(2008) == True 
     assert is_leap_year(2012) == True 
     
